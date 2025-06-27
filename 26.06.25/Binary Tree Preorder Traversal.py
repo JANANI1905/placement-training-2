33cdecc563d89ebcdb0110b1c7be9fb74abba41b
JANANI1905 <151322389+JANANI1905@users.noreply.github.com>
2025-06-27 21:39:56 +0530

    Create Binary Tree Preorder Traversal.py

---
+++ 26.06.25/Binary Tree Preorder Traversal.py
@@ -0,0 +1,19 @@
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        ans = []
+        while root:
+            if root.left is None:
+                ans.append(root.val)
+                root = root.right
+            else:
+                prev = root.left
+                while prev.right and prev.right != root:
+                    prev = prev.right
+                if prev.right is None:
+                    ans.append(root.val)
+                    prev.right = root
+                    root = root.left
+                else:
+                    prev.right = None
+                    root = root.right
+        return ans
